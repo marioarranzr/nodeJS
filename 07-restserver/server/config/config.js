db4cdf6c4251c3a4e259f61555ec6b6c855b8384
Mario Arranz <mario.arranz@yoti.com>
2018-04-19 11:36:36 +0100

    enviroment variable mongo

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -15,6 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:123456@ds151169.mlab.com:51169/cafedb'
+    urlDB = process.env.MONGO_URL;
 }
 process.env.urlDB = urlDB;
